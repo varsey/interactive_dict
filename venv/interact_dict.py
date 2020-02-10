3b63a8bbd83b8b0229514aece7af7c7fe7adf0f5
varsey <ravvin16@yandex.ru>
2020-02-10 22:54:14 +0300

    compelted tutorial

--- venv/interact_dict.py
+++ venv/interact_dict.py
@@ -1,7 +1,4 @@
-# inspired by https://telegra.ph/Sozdayom-prilozhenie-slovar-na-Python-02-06
-
 import json
-import difflib
 from difflib import get_close_matches
 
 data = json.load(open("dictionary.json"))
@@ -15,7 +12,6 @@ def retrive_definition(word):
         return data[word.title()]
     elif word.upper() in data:
         return data[word.upper()]
-
     elif len(get_close_matches(word, data.keys())) > 0:
         action = input("Did you mean %s instead? [y or n]: " % get_close_matches(word, data.keys())[0])
         if (action == "y"):
@@ -27,4 +23,10 @@ def retrive_definition(word):
 
 word_user = input("Enter a word: ")
 
-print(retrive_definition(word_user))
+output = retrive_definition(word_user)
+
+if type(output) == list:
+    for item in output:
+        print("-",item)
+else:
+    print("-",output)
