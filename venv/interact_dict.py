2047acab32353d0034770b279dea6aab3266fbe6
varsey <ravvin16@yandex.ru>
2020-02-13 22:55:23 +0300

    deleted extra info

--- venv/interact_dict.py
+++ venv/interact_dict.py
@@ -13,13 +13,13 @@ def retrive_definition(word):
     elif word.upper() in data:
         return data[word.upper()]
     elif len(get_close_matches(word, data.keys())) > 0:
-        action = input("Did you mean %s instead? [y or n]: " % get_close_matches(word, data.keys())[0])
-        if (action == "y"):
-            return data[get_close_matches(word, data.keys())[0]]
-        elif (action == "n"):
-            return ("The word doesn't exist, yet.")
-        else:
-            return ("We don't understand your entry. Apologies.")
+        print("We found %s instead: " % get_close_matches(word, data.keys())[0])
+#        if (action == "y"):
+        return data[get_close_matches(word, data.keys())[0]]
+#        elif (action == "n"):
+#            return ("The word doesn't exist, yet.")
+#        else:
+#            return ("We don't understand your entry. Apologies.")
 
 word_user = input("Enter a word: ")
 
